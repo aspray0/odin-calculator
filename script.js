3c29ec736bf99464890c7340449e5b8e086bfef1
aspray0 <andrew.d.spray@gmail.com>
2022-12-03 14:53:10 -0800

    Code basic math op functions

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // script.js
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, num1, num2) {
+  switch (true) {
+    case (operator === '+'):
+      return add(num1, num2);
+    case (operator === '-'):
+      return subtract(num1, num2);
+    case (operator === '*'):
+      return multiply(num1, num2);
+    case (operator === '/'):
+      return divide(num1, num2);
+  }
+}
